0af9db7866506e7644643c988ea2740d257d618b
keef <limy0313@e.ntu.edu.sg>
2023-03-23 16:18:38 +0800

    frontend done

--- full_stack_blog/src/pages/ArticlePage.js
+++ full_stack_blog/src/pages/ArticlePage.js
@@ -1,10 +1,16 @@
 import { useParams } from "react-router-dom";
 import articles from "./Article-Content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
   const params = useParams();
   const articleId = params.articleId; // const { articleId} = useParams();
   const article = articles.find((article) => article.name === articleId);
+
+  if (!article) {
+    return <NotFoundPage />;
+  }
+
   return (
     <>
       <h1>{article.title}</h1>
