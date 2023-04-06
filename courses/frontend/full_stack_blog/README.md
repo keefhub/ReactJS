038ef31479c23cbc3dbce75d8f76377d72439749
keefhub <limhehe02761@gmail.com>
2023-04-06 22:50:30 +0800

    some edits

--- courses/frontend/full_stack_blog/README.md
+++
@@ -1,2 +0,0 @@
-#First Full Stack Blog
-This app is the first full stack app that i will be building. It is based off a course from LinkedIn Learning
--- courses/frontend/src/pages/ArticlePage.js
+++ courses/frontend/src/pages/ArticlePage.js
@@ -1,8 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import articles from "./Article-Content";
 import NotFoundPage from "./NotFoundPage";
 
+//useState stores memories, useEffect adds logic to components like loading data from server
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: 3, comments: [] });
+  });
+
   const params = useParams();
   const articleId = params.articleId; // const { articleId} = useParams();
   const article = articles.find((article) => article.name === articleId);
@@ -14,6 +22,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>this article has {articleInfo.upvotes} upvote</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
